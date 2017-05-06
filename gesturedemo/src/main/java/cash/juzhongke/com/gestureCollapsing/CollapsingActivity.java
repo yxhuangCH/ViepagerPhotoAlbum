39c86b154952d8e5d92137276b231ec6fc8de0ad
yxhuang <hyx@gdysj.com.cn>
2017-05-06 18:12:46 +0800

    add some

--- gesturedemo/src/main/java/cash/juzhongke/com/gestureCollapsing/CollapsingActivity.java
+++ gesturedemo/src/main/java/cash/juzhongke/com/gestureCollapsing/CollapsingActivity.java
@@ -2,15 +2,19 @@ package cash.juzhongke.com.gestureCollapsing;
 
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import cash.juzhongke.com.gesturedemo.R;
 
 public class CollapsingActivity extends AppCompatActivity {
+
+    private static final String TAG = CollapsingActivity.class.getSimpleName();
 
     private float mSelfHeight = 0;//用以判断是否得到正确的宽高值
     private float mTitleScale;
@@ -22,7 +26,8 @@ public class CollapsingActivity extends AppCompatActivity {
     TextView mSubscriptionTitle;
     TextView mSubscribe;
     AppBarLayout mAppBar;
-    Toolbar mToolbar;
+    LinearLayout ll_content;
+    CoordinatorLayout coordinator_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +39,8 @@ public class CollapsingActivity extends AppCompatActivity {
         mSubscriptionTitle = (TextView) findViewById(R.id.subscription_title);
         mSubscribe = (TextView) findViewById(R.id.subscribe);
         mAppBar = (AppBarLayout) findViewById(R.id.app_bar);
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        ll_content = (LinearLayout) findViewById(R.id.ll_content);
+        coordinator_layout = (CoordinatorLayout) findViewById(R.id.coordinator_layout);
 
         final float screenW = getResources().getDisplayMetrics().widthPixels;
         final float toolbarHeight = 0;
@@ -53,15 +59,46 @@ public class CollapsingActivity extends AppCompatActivity {
                     mHeadImgScale = distanceHeadImg / (initHeight - toolbarHeight);
                     mSubScribeScaleX = distanceSubscribeX / (initHeight - toolbarHeight);
                 }
-                float scale = 1.0f - (-verticalOffset) / (initHeight - toolbarHeight);
-                mHeadImage.setScaleX(scale);
-                mHeadImage.setScaleY(scale);
+
+
+                float scale = (1.0f + (-verticalOffset) / (initHeight - toolbarHeight)) * 5f;
+
+                Log.e(TAG, " verticalOffset " + verticalOffset);
+
+                verticalOffset = verticalOffset <= -100 ? -100 : verticalOffset;
+
+                float converScale = converScale(verticalOffset);
+
+                mHeadImage.setScaleX(converScale);
+                mHeadImage.setScaleY(converScale);
+
                 mHeadImage.setTranslationY(mHeadImgScale * verticalOffset);
                 mSubscriptionTitle.setTranslationY(mTitleScale * verticalOffset);
                 mSubscribe.setTranslationY(mSubScribeScale * verticalOffset);
                 mSubscribe.setTranslationX(-mSubScribeScaleX * verticalOffset);
+
+                float scaleAlpha = 1.0f - (-verticalOffset) / (initHeight - toolbarHeight);
+
+                Log.i(TAG, " scale " + scale + "  verticalOffset " + verticalOffset + " scaleAlpha: " + scaleAlpha +
+                            "  mHeadImgScale: " + mHeadImgScale  + "  mSelfHeight: " + mSelfHeight + "  converScale: " + converScale);
+
+                coordinator_layout.setAlpha(scaleAlpha);
+
             }
         });
+
+    }
+
+    /**
+     *  根据距离换算成 Scale
+     * @param verticalOffset
+     * @return
+     *  距离 [0,  -100]
+     *  scale [5.0, 7.0]
+     */
+    private float converScale(int verticalOffset){
+        verticalOffset = Math.abs(verticalOffset) >= 100 ? 100 : verticalOffset;
+        return 0.02f * verticalOffset;
     }
 
 }
